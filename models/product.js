7d92b18c428e12f12fe3618cb56f4043a62a7f30
darshhbad <darsh.badukle.che17@itbhu.ac.in>
2023-04-03 19:09:43 +0530

    first commit

---
+++ models/product.js
@@ -0,0 +1,40 @@
+const fs=require('fs')
+
+const path=require('path')
+
+const getProductfromfile=(cb)=>{
+    const p =path.join(path.dirname(process.mainModule.filename),'data','products.json')
+
+    fs.readFile(p,(err,fileContent)=>{
+        if (err){
+            return cb([])
+        }
+        else{
+        cb(JSON.parse(fileContent))
+        }
+    })
+
+    
+}
+
+module.exports=class Product{
+    constructor(t){
+        this.title=t;
+
+    }
+    save(){
+        getProductfromfile(products=>{
+            
+            products.push(this)
+            fs.writeFile(p,JSON.stringify(products),(err)=>{
+                console.log(err)
+            })
+        })
+        const p =path.join(path.dirname(process.mainModule.filename),'data','products.json')
+        
+    }
+
+    static fetchAll(cb){
+        getProductfromfile(cb)
+    }
+}
